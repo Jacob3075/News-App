d522db5e842c07ee549967386b6426943865cfa7
Jacob Bosco <jacob.bosco3075@gmail.com>
2021-03-28 15:12:42 +0530

    Changed ArticleCard to use Constraint layout instead of Relative. Fixed MainActivity xml.

--- app/src/main/java/com/jacob/newsapp/ui/fragments/HomePage.java
+++ app/src/main/java/com/jacob/newsapp/ui/fragments/HomePage.java
@@ -1,5 +1,6 @@
 package com.jacob.newsapp.ui.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,7 +8,9 @@ import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
 
+import com.google.android.material.button.MaterialButton;
 import com.jacob.newsapp.R;
+import com.jacob.newsapp.ui.ArticleCard;
 
 public class HomePage extends Fragment {
 
@@ -19,6 +22,14 @@ public class HomePage extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home_page, container, false);
+        View view = inflater.inflate(R.layout.fragment_home_page, container, false);
+        MaterialButton button = view.findViewById(R.id.button);
+        button.setOnClickListener(this::openArticleCard);
+        return view;
+    }
+
+    private void openArticleCard(View onClick) {
+        Intent intent = new Intent(getActivity(), ArticleCard.class);
+        startActivity(intent);
     }
 }
