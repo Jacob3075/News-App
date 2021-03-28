3eb472b1c5718e8fc3bf8edd4ed8f2986143e76e
Jacob Bosco <jacob.bosco3075@gmail.com>
2021-03-28 14:20:23 +0530

    Added logging to RetrofitService.

--- app/src/main/java/com/jacob/newsapp/services/RetrofitService.java
+++ app/src/main/java/com/jacob/newsapp/services/RetrofitService.java
@@ -1,5 +1,9 @@
 package com.jacob.newsapp.services;
 
+import org.jetbrains.annotations.NotNull;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -7,13 +11,29 @@ import static com.jacob.newsapp.utilities.Constants.BASE_URL;
 
 public class RetrofitService {
 
-    private static final Retrofit retrofit = new Retrofit.Builder()
-            .baseUrl(BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build();
+    private static Retrofit retrofit;
 
     public static ArticleAPI getInterface() {
+        if (retrofit == null) {
+            OkHttpClient okHttpClient = getOkHttpClient();
+
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .client(okHttpClient)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
         return retrofit.create(ArticleAPI.class);
+    }
+
+    @NotNull
+    private static OkHttpClient getOkHttpClient() {
+        OkHttpClient.Builder builder = new OkHttpClient.Builder();
+        HttpLoggingInterceptor httpLoggingInterceptor = new HttpLoggingInterceptor();
+        httpLoggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BASIC);
+        builder.networkInterceptors().
+                add(httpLoggingInterceptor);
+        return builder.build();
     }
 
 }
