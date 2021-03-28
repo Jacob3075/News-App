0d4da34534727a74d9c7f5fd99a512a1463b7c54
Jacob Bosco <jacob.bosco3075@gmail.com>
2021-03-28 14:22:46 +0530

    Added HomePageViewModal and used it in HomePage fragment.

--- app/src/main/java/com/jacob/newsapp/ui/fragments/HomePage.java
+++ app/src/main/java/com/jacob/newsapp/ui/fragments/HomePage.java
@@ -5,16 +5,29 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.Nullable;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.Navigation;
 
 import com.jacob.newsapp.R;
 import com.jacob.newsapp.databinding.FragmentHomePageBinding;
+import com.jacob.newsapp.models.Article;
+import com.jacob.newsapp.models.MediaStackResponse;
+import com.jacob.newsapp.viewmodels.HomePageViewModal;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomePage extends Fragment {
 
     private FragmentHomePageBinding binding;
+    private HomePageViewModal viewModel;
+    private List<Article> articles = new ArrayList<>();
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -22,12 +35,42 @@ public class HomePage extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NotNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         binding = FragmentHomePageBinding.inflate(inflater, container, false);
         ConstraintLayout root = binding.getRoot();
-        binding.btnOpenArticle.setOnClickListener(view -> Navigation.findNavController(view).navigate(R.id.action_homePage_to_articleViewer));
+
+//        binding.btnOpenArticle.setOnClickListener(this::openArticle);
+        binding.btnOpenArticle.setOnClickListener(this::getArticlesFromSource);
         return root;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        viewModel = new ViewModelProvider(requireActivity()).get(HomePageViewModal.class);
+
+        MutableLiveData<MediaStackResponse> newsBySource = viewModel.getNewsBySource("bbc");
+        newsBySource.observe(getViewLifecycleOwner(), mediaStackResponse -> {
+            List<Article> articles = mediaStackResponse.getArticles();
+            articles.stream()
+                    .limit(15)
+                    .forEach(System.out::println);
+        });
+    }
+
+    private void openArticle(View view) {
+        Navigation.findNavController(view).navigate(R.id.action_homePage_to_articleViewer);
+    }
+
+    private void getArticlesFromSource(View view) {
+        MutableLiveData<MediaStackResponse> newsBySource = viewModel.getTrendingNews();
+        newsBySource.observe(getViewLifecycleOwner(), mediaStackResponse -> {
+            List<Article> articles = mediaStackResponse.getArticles();
+            articles.stream()
+                    .limit(15)
+                    .forEach(System.out::println);
+        });
     }
 
     @Override
