176a0a92954fc0d402e7c8c9ee50d11c78c07c5e
Jacob Bosco <jacob.bosco3075@gmail.com>
2021-04-02 14:23:12 +0530

    Fixed search bar styling.

--- app/src/main/java/com/jacob/newsapp/viewmodels/tabs/SearchArticlesTabViewModel.java
+++
@@ -1,7 +0,0 @@
-package com.jacob.newsapp.viewmodels.tabs;
-
-import androidx.lifecycle.ViewModel;
-
-public class SearchArticlesTabViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
-}
--- app/src/main/java/com/jacob/newsapp/viewmodels/tabs/SearchSourcesTabViewModel.java
+++
@@ -1,7 +0,0 @@
-package com.jacob.newsapp.viewmodels.tabs;
-
-import androidx.lifecycle.ViewModel;
-
-public class SearchSourcesTabViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
-}
