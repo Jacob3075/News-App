88e457ec8ef54b2fe0fbb49772421a8c880c4ea9
akif-iqbal <akif.iqbal28@gmail.com>
2021-04-04 16:40:03 +0530

    Partly finished RecyclerView for SavedCategoriesAdapter.java and SavedSourcesAdapter.java Requires some corrections

---
+++ app/src/main/java/com/jacob/newsapp/adapters/SavedCategoriesAdapter.java
@@ -0,0 +1,50 @@
+package com.jacob.newsapp.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.jacob.newsapp.R;
+
+public class SavedCategoriesAdapter extends RecyclerView.Adapter<SavedCategoriesAdapter.MyViewHolder> {
+
+    String[] data;
+    Context context;
+
+    public SavedCategoriesAdapter(Context ct, String[] s1) {
+        context = ct;
+        data = s1;
+    }
+
+    @NonNull
+    @Override
+    public SavedCategoriesAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.activity_saved_category, parent, false);
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        holder.txtCategory.setText(data[position]);
+    }
+
+    @Override
+    public int getItemCount() {
+        return data.length;
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        TextView txtCategory;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            txtCategory = itemView.findViewById(R.id.txtCategory);
+        }
+    }
+}
