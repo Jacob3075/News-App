efcb965e1c3c53c348e5fc3a4ce4b14b757005e0
Jacob Bosco <jacob.bosco3075@gmail.com>
2021-04-16 14:14:33 +0530

    Changed PAGE_SIZE constant to a higher value to reduce API calls.

--- app/src/main/java/com/jacob/newsapp/utilities/Constants.java
+++ app/src/main/java/com/jacob/newsapp/utilities/Constants.java
@@ -2,10 +2,14 @@ package com.jacob.newsapp.utilities;
 
 public class Constants {
 
+    public static final int PAGE_SIZE = 50;
+
     public static final String BASE_URL = "http://api.mediastack.com/v1/";
     // public static final String API_KEY          = "0fef1f52555a6cc680c13fbfd63c740c";
     public static final String API_KEY = "4f2cf965eefe507ba80b8fe8678342f8";
     public static final String LANGUAGE_ENGLISH = "en";
-    public static final int PAGE_SIZE = 25;
+
+    private Constants() {}
+
     public static final String USERS = "users";
 }
